bb4d882792eba7341a20d0a8c880082b4ccdc7ed
hmen97 <hmenezes36@gmail.com>
2020-02-25 18:22:19 +0530

    replaced librosa with tensorflow

--- converttowav.py
+++ converttowav.py
@@ -4,71 +4,51 @@ import os
 import argparse
 import multiprocessing
 from multiprocessing import Process
-
+import pandas as pd
 from pydub import AudioSegment
-
 formats_to_convert = ['.m4a']
 
-def convert():
-    for (dirpath, dirnames, filenames) in os.walk("wav/"):
-        for filename in filenames:
-            if filename.endswith(tuple(formats_to_convert)):
 
-                filepath = dirpath + '/' + filename
-                (path, file_extension) = os.path.splitext(filepath)
-                file_extension_final = file_extension.replace('.', '')
-                try:
-                    track = AudioSegment.from_file(filepath,
-                                                   file_extension_final)
-                    wav_filename = filename.replace(file_extension_final, 'wav')
-                    wav_path = dirpath + '/' + wav_filename
-                    print('CONVERTING: ' + str(filepath))
-                    file_handle = track.export(wav_path, format='wav')
-                    os.remove(filepath)
-                except:
-                    print("ERROR CONVERTING " + str(filepath))
+def set_mp(processes=20):
+    import multiprocessing as mp
+
+    def init_worker():
+        import signal
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    global pool
+    try:
+        pool.terminate()
+    except:
+        pass
+
+    if processes:
+        pool = mp.Pool(processes=processes, initializer=init_worker)
+    else:
+        pool = None
+    return pool
+
+
+def convert(filepath):
+    (path, file_extension) = os.path.splitext(filepath)
+    file_extension_final = file_extension.replace('.', '')
+    filename = filepath.split("/")[-1]
+    dirpath = filepath.split["/"][:-1]
+    try:
+        track = AudioSegment.from_file(filepath,
+                                       file_extension_final)
+        wav_filename = filename.replace(file_extension_final, 'wav')
+        wav_path = dirpath + '/' + wav_filename
+        print('CONVERTING: ' + str(filepath))
+        file_handle = track.export(wav_path, format='wav')
+        os.remove(filepath)
+    except:
+        print("ERROR CONVERTING " + str(filepath))
+
 
 if __name__=="__main__":
-    p1 = Process(target=convert)
-    p2 = Process(target=convert)
-    p3 = Process(target=convert)
-    p4 = Process(target=convert)
-    p5 = Process(target=convert)
-    p6 = Process(target=convert)
-    p7 = Process(target=convert)
-    p8 = Process(target=convert)
-    p9 = Process(target=convert)
-    p10 = Process(target=convert)
-    p11 = Process(target=convert)
-    p12 = Process(target=convert)
-    p13 = Process(target=convert)
-    p14 = Process(target=convert)
-    p1.start()
-    p2.start()
-    p3.start()
-    p4.start()
-    p5.start()
-    p6.start()
-    p7.start()
-    p8.start()
-    p9.start()
-    p10.start()
-    p11.start()
-    p12.start()
-    p13.start()
-    p14.start()
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
-    p5.join()
-    p6.join()
-    p7.join()
-    p8.join()
-    p9.join()
-    p10.join()
-    p11.join()
-    p12.join()
-    p13.join()
-    p14.join()
-    print("We're done")
+    pool = set_mp(processes=20)
+    files = list(pd.read_csv("../meta/data.txt", header=None)[0])
+    pool.apply_async(convert, args=(files, ))
+
+

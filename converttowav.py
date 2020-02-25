76574c2cd1257f56b7548afffc4ffca31adc00ca
hmen97 <hmenezes36@gmail.com>
2020-02-25 18:40:32 +0530

    replaced librosa with tensorflow

--- converttowav.py
+++ converttowav.py
@@ -1,39 +1,18 @@
 # Convert m4a extension files to wav extension files
 
 import os
-import argparse
-import multiprocessing
-from multiprocessing import Process
+from multiprocessing import Pool
 import pandas as pd
 from pydub import AudioSegment
 formats_to_convert = ['.m4a']
 
-
-def set_mp(processes=20):
-    import multiprocessing as mp
-
-    def init_worker():
-        import signal
-        signal.signal(signal.SIGINT, signal.SIG_IGN)
-
-    global pool
-    try:
-        pool.terminate()
-    except:
-        pass
-
-    if processes:
-        pool = mp.Pool(processes=processes, initializer=init_worker)
-    else:
-        pool = None
-    return pool
 
 
 def convert(filepath):
     (path, file_extension) = os.path.splitext(filepath)
     file_extension_final = file_extension.replace('.', '')
     filename = filepath.split("/")[-1]
-    dirpath = filepath.split["/"][:-1]
+    dirpath = filepath.split("/")[:-1]
     try:
         track = AudioSegment.from_file(filepath,
                                        file_extension_final)
@@ -47,8 +26,9 @@ def convert(filepath):
 
 
 if __name__=="__main__":
-    pool = set_mp(processes=20)
-    files = list(pd.read_csv("../meta/data.txt", header=None)[0])
-    pool.apply_async(convert, args=(files, ))
+    files = list(pd.read_csv("data.txt", header=None)[0])
+    print(len(files))
+    with Pool(20) as p:
+        print(p.map(convert, files))
 
 
